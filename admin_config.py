7e5a0650cf3acb3994239c3fdc083b720c854a02
robinlin <yuanhuilin@91jkys.com>
2019-11-14 10:39:13 +0800

    提交管理员配置信息

---
+++ admin_config.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+release = True
+token = ''
+appId = ''
+appSecret = ''
+admin_name = ''
+if release:
+    token = ''
+    appId = ''
+    appSecret = ''
+    admin_name = ''
+else:
+    token = ''
+    appId = ''
+    appSecret = ''
+    admin_name = ''
+
+admin_phone_list = ['']
+admin_list = ['']
